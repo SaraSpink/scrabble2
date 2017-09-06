7c6fef24761335ddb199d606d5461137129cee0f
Javier Jimenez and Sara Spink <sara@>
2017-09-06 16:28:13 -0700

    add a class and an inheritance

---
+++ spec/scrabble2_spec.rb
@@ -0,0 +1,21 @@
+require('rspec')
+require('scrabble2')
+
+describe('String#scrabble_score') do
+  it("returns a scrabble score for a letter") do
+    expect(("a").scrabble_score()).to(eq(1))
+  end
+
+  it("returns a scrabble score for a word") do
+    expect(("adbfkjq").scrabble_score()).to(eq(33))
+  end
+
+  it("returns a scrabble score for a word") do
+    expect(("hello").scrabble_score()).to(eq(8))
+  end
+
+  it("returns a scrabble score for a sentence") do
+    expect(("sara has a dog").scrabble_score()).to(eq(16))
+  end
+
+end
